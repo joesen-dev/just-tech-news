57f7cdddd048b71afae0cff8056e5aa1c3433a0d
Joe <105400893+joesen-dev@users.noreply.github.com>
2022-08-16 16:40:03 -0400

    Updates to feature/post - Define Model Associations - Create API routes for the Post Model

--- models/index.js
+++ models/index.js
@@ -1,4 +1,17 @@
 const User = require("./User");
 const Post = require("./Post");
 
+// create associations
+// * this is a one-to-many relationship
+// * one User to many Posts
+User.hasMany(Post, {
+  foreignKey: "user_id",
+});
+
+// * this is a one-to-one relationship
+// * one Posts to one User
+Post.belongsTo(User, {
+  foreignKey: "user_id",
+});
+
 module.exports = { User, Post };
--- routes/api/index.js
+++ routes/api/index.js
@@ -2,8 +2,10 @@ const router = require("express").Router();
 
 // collect the user-routes.js endpoints
 const userRoutes = require("./user-routes");
+const postRoutes = require("./post-routes");
 
 // prefix /users to routes from user-routes.js
 router.use("/users", userRoutes);
+router.use("/posts", postRoutes);
 
 module.exports = router;
